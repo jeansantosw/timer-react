58c18308ebf926b0978ca20a2a1073aa42f7153d
Jean Santos <jeansantos.sw@gmail.com>
2023-01-31 19:04:08 +0000

    React router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { ThemeProvider } from 'styled-components'
-
+import { BrowserRouter } from 'react-router-dom'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
-
