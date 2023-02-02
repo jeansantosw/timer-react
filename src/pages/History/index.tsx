cf485bedf668859b8971e1443f697fad9ac6e017
Jean Santos <jeansantos.sw@gmail.com>
2023-02-02 16:02:33 +0000

    Formulárioi

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,47 @@
+import { HistoryContainer, HistoryList } from './style'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>30 minutos</td>
+              <td>Há 2 dias</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>30 minutos</td>
+              <td>Há 2 dias</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>30 minutos</td>
+              <td>Há 2 dias</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>30 minutos</td>
+              <td>Há 2 dias</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
