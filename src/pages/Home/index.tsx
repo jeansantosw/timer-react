9e1643c1297f7ab36e477458a4a51eefd8d8549f
Jean Santos <jeansantos.sw@gmail.com>
2023-02-02 15:30:39 +0000

    Aprimorando os inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,55 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+          <datalist id="task-suggestions">
+            <option value="Task 1" />
+            <option value="Task 2" />
+            <option value="Task 3" />
+            <option value="Task 4" />
+          </datalist>
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+        <StartCountdownButton disabled type="submit">
+          <Play />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
 }
