936501bb319c279a9f7fda4abd12b61576252af1
Jean Santos <jeansantos.sw@gmail.com>
2023-02-03 17:08:19 +0000

    Iniciando um novo cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -27,6 +27,8 @@ interface Cycle {
   minutesAmount: number
 }
 
+// END INTERFACE
+
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
   minutesAmount: zod.number().min(5).max(60),
